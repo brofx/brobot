8618c3bc6bd9edf6cd4b15563c5c4e795cde0d9d
Mvalliere <mark.valliere1@gmail.com>
2019-03-05 17:04:20 -0500

    Add files via upload
    Run this file with Node to start the bot

---
+++ index.js
@@ -0,0 +1,27 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+
+
+
+
+
+client.on('message', message => {
+        if (message.content === '!ping') {
+                message.channel.send('Pong.');
+        }
+});
+
+
+
+
+
+
+
+
+client.login('SECRETKEY');

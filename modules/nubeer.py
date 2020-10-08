35e65802e7ebb94a47758701a4dc1108df7d8480
Scott Adie <scott@adie.io>
2020-10-08 19:30:59 -0400

    Fixed key error

--- modules/nubeer.py
+++ modules/nubeer.py
@@ -7,14 +7,14 @@ NUBEER_STATS_URL = 'https://nubeer.io/api/3812-52134-452148-0482134'
 class Nubeer(commands.Cog, name="Nubeer Information"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
     @commands.command()
     @commands.guild_only()
-
     async def nubeer(self, ctx: commands.Context):
         """Displays Nubeer Stats"""      
         embed = get_nubeer_stats()  
         if embed is None:
-            await ctx.send("Unable to get stats.")
+            return await ctx.send("Unable to get stats.")
         return await ctx.send(embed=embed)
 
 def get_nubeer_stats():
@@ -22,11 +22,11 @@ def get_nubeer_stats():
 
     if api_response.ok:
         data: dict = api_response.json()["data"]
-        if data["data"] is not None: 
-            beers = data["beers"],
-            users = data["users"],
-            breweries = data["breweries"],
-            ratings = data["ratings"],
+        if data is not None: 
+            beers = data["beers"]
+            users = data["users"]
+            breweries = data["breweries"]
+            ratings = data["ratings"]
 
             embed: discord.Embed = discord.Embed(title="Nubeer Stats")
 

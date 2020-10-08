94088014d06024bed3be79e52defb52fb1a20e6a
Seth <matvmp7@gmail.com>
2020-10-08 19:22:27 -0400

    fixed reviewed issues

--- modules/nubeer.py
+++ modules/nubeer.py
@@ -7,18 +7,15 @@ NUBEER_STATS_URL = 'https://nubeer.io/api/3812-52134-452148-0482134'
 class Nubeer(commands.Cog, name="Nubeer Information"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
     @commands.command()
     @commands.guild_only()
 
     async def nubeer(self, ctx: commands.Context):
         """Displays Nubeer Stats"""      
         embed = get_nubeer_stats()  
+        if embed is None:
+            await ctx.send("Unable to get stats.")
         return await ctx.send(embed=embed)
-
-
-def setup(bot: commands.Bot):
-    bot.add_cog(Nubeer(bot))
 
 def get_nubeer_stats():
     api_response = requests.get(NUBEER_STATS_URL)
@@ -45,3 +42,5 @@ def get_nubeer_stats():
 
     return None
 
+def setup(bot: commands.Bot):
+    bot.add_cog(Nubeer(bot))

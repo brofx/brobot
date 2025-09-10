8310b91eec318648801db62bd0112d6bc6fe3a74
Scott Adie <scott@adie.io>
2025-09-10 17:27:02 -0400

    Fixes sigma

--- modules/spin.py
+++ modules/spin.py
@@ -1181,8 +1181,8 @@ class SlotsCog(commands.Cog):
         desc = (
             f"🗡️ <@{uid}> has issued a **1v1 challenge**!\n"
             f"Join cost: **{init_fee:,}** ({fmt_spin_value(init_fee)}).\n"
-            f"Minimum payout: **{init_fee*2:,}** ({fmt_spin_value(init_fee*2)})"
-            f"Expires **<t:{expires_at}:R>**.\n"
+            f"Minimum payout: **{init_fee*2:,}** ({fmt_spin_value(init_fee*2)})\n"
+            f"Expires **<t:{expires_at}:R>**."
         )
         embed = discord.Embed(title="1v1 Challenge", description=desc, color=discord.Color.blurple())
 
